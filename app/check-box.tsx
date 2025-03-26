3df3016d292631b118cc143b5fca9f182d611afb
Miguel Zavala <Miguelazavala1013@gmail.com>
2025-03-26 00:27:38 -0400

    added query parameters and selector for number of cards to study

--- app/check-box.tsx
+++ app/check-box.tsx
@@ -8,13 +8,14 @@ interface checkBoxProps {
 
 export default function CheckBox({label, isChecked, onClick}: checkBoxProps) {
     return (
+        // clicking anywhere on the div checks the box
         <div className="flex flex-row rounded-md items-center h-24 w-56 bg-gradient-to-tr from-red-600 to-orange-600 m-4" onClick={onClick}>
             <input className="hidden" type="checkbox" id={label} checked={isChecked} onChange={e => {
                 e.stopPropagation();
                 onClick();
             }
             }/>
-            <label className="cursor-pointer  h-8 w-8 border-2 border-black bg-white mx-2 p-1"  htmlFor={label} onClick={e => e.stopPropagation()}> {!isChecked && <img className="h-4 w-4" src="/check-mark.svg" alt="."/>} </label>
+            <label className="cursor-pointer  h-8 w-8 border-2 border-black bg-white mx-2 p-1"  htmlFor={label} onClick={e => e.stopPropagation()}> {isChecked && <img className="h-4 w-4" src="/check-mark.svg" alt="."/>} </label>
 
             <MathJax className="text-white font-serif">{label}</MathJax>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import CheckBox from "./check-box";
 import { MathJaxContext } from "better-react-mathjax";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface checkBoxData {
   id: number;
@@ -11,8 +12,12 @@ interface checkBoxData {
 }
 
 export default function Home() {
-  const [checked, setChecked] = useState(Array(10).fill(false));
 
+  const [checked, setChecked] = useState(Array(10).fill(true));
+
+  const [cards, setCards] = useState(0);
+
+  // categories for integrals
   const Checkboxes: checkBoxData[] = [
     {id: 0, label: "Basic Forms"},
     {id: 1, label: "Rational Function"},
@@ -25,7 +30,10 @@ export default function Home() {
     {id: 8, label: "Trigonometric Functions with \\(x^n\\) and \\(e^{ax}\\)"},
     {id: 9, label: "Hyperbolic Functions"}
   ];
+
+  const router= useRouter();
  
+  // handles checking the category boxes
   const handleClick = (id: number) => {
     const nextChecked: boolean[] = checked.map((c, i) => {
       if (i === id) {
@@ -37,28 +45,57 @@ export default function Home() {
     setChecked(nextChecked);
   };
 
+  // uses query parameters track categories and number of cards between pages
+  const handleStudyClick = () => {
+    const params = new URLSearchParams();
+
+    Checkboxes.forEach(checkbox => {
+      if (checked[checkbox.id]) {
+        params.append("categories", checkbox.label);
+      }
+    });
+
+    params.append("cards", cards.toString());
+
+    router.push(`/flashcards?${params.toString()}`)
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value: number = parseInt(e.target.value);
+    setCards(value);
+  }
+
   return (
     <MathJaxContext>
       <div className="h-full bg-slate-300">
+
         <div className="text-center text-xl font-serif mx-4 mb-2 underline decoration-black decoration-2">Table of Integrals taken from
           <a className="italic font-semibold" href="https://www.physics.umd.edu/hep/drew/IntegralTable.pdf" target="_blank"> HERE</a>
         </div>
+
         <h3 className="pt-24 text-center text-2xl font-semibold font-serif">Select Which Kind of Integrals to Show</h3>
+
         <div className="grid grid-cols-5 content-stretch justify-items-stretch place-items-stretch py-12 mx-24 my-2 bg-white">
           {Checkboxes.map((checkbox) => 
             <CheckBox  key={checkbox.id} label={checkbox.label} onClick={() => handleClick(checkbox.id)} isChecked={checked[checkbox.id]} />
           )}
         </div>
+
         <div className="flex flex-col h-auto mt-4">
+
           <div className="flex-1 flex justify-center">
-            <Link href={"/flashcards"} className="bg-gradient-to-tr from-rose-700 to-orange-700 text-white px-4 py-2 rounded-lg font-serif font-medium text-3xl"> STUDY </Link>
+            <input type="number" name="cards" id="cards" value={cards} onChange={handleChange} />
+            <button onClick={handleStudyClick}>Study</button>
           </div>
+
           <div className="flex-1 flex justify-start">
             <Link href="https://github.com/MiguelZ13" target="_blank">
               <img className="h-12 w-12 mt-32 mb-12 mx-6" src="/github-mark.svg" alt="Github Logo" />
             </Link>
           </div>
+
         </div>
+
       </div>
     </MathJaxContext>
   );
